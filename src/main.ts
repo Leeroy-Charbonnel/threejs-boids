7afe2f6adec8bf58b8efcf9533bd9da4c716790c
LeeroyCharbonnel <leeroy.charbonnel@stms.fr>
2025-09-02 16:28:26 +0200

    Add three js basic cube scene

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene=new THREE.Scene();
+const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+const renderer=new THREE.WebGLRenderer();
+
+const controls=new OrbitControls(camera,renderer.domElement);
+
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+const geometry=new THREE.BoxGeometry(1,1,1);
+const material=new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube=new THREE.Mesh(geometry,material);
+scene.add(cube);
+
+camera.position.z=5;
+controls.update();
+
+function animate() {
+    controls.update();
+    renderer.render(scene,camera);
+}
